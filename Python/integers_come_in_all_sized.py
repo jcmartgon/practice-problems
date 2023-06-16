77ee95506bab002fd767fef2a464402e14424097
JCMartGon <jcmartgon@gmail.com>
2023-06-15 23:34:01 -0600

    Day 21

---
+++ Python/integers_come_in_all_sized.py
@@ -0,0 +1,15 @@
+# Jesus Carlos Martinez Gonzalez
+# 15/06/2023
+# Integers Come in All Sizes (https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem)
+
+"""
+Read four numbers, a, b, c, and d, and print the result of a^b + c^d.
+"""
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+
+    print(a**b + c**d)
