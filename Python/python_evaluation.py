aa0c9d524d4790caf83cd987299eb49cc67914fe
JCMartGon <jcmartgon@gmail.com>
2023-06-21 23:08:13 -0600

    Day 25

---
+++ Python/python_evaluation.py
@@ -0,0 +1,11 @@
+# Jesus Carlos Martinez Gonzalez
+# 21/06/2023
+# Python Evaluation (https://www.hackerrank.com/challenges/python-eval/problem)
+
+"""
+You are given an expression in a line. Read that line as a string variable, such as var, and print the result using eval(var).
+"""
+
+if __name__ == "__main__":
+    inpt = input()
+    eval(inpt)
