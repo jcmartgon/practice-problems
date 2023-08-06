c868c712b76ce1bed21344c98901c6b120070ee4
JCMartGon <jcmartgon@gmail.com>
2023-08-05 23:46:26 -0600

    Day 65

---
+++ Python/kitchen_timings.py
@@ -0,0 +1,13 @@
+# Jesus Carlos Martinez Gonzalez
+# 05/08/2023
+# Kitchen timings
+
+"""
+The working hours of Chefs kitchen are from X pmto Y pm (I X < Y 12).
+Find the number of hours Chef works.
+"""
+
+t = int(input())
+for _ in range(t):
+    x, y = map(int, input().split())
+    print(y - x)
