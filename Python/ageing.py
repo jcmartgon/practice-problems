18ee09bfa3f2befd80cc99a98cb4af51c902801b
JCMartGon <jcmartgon@gmail.com>
2023-08-08 22:52:47 -0600

    Day 68

---
+++ Python/ageing.py
@@ -0,0 +1,12 @@
+# Jesus Carlos Martinez Gonzalez
+# 08/08/2023
+# Ageing (https://www.codechef.com/problems/AGEING)
+
+"""
+Chef's current age is 20 years, while Chefina's current age is IO years.
+Determine Chefinars age when Chef will be X years old.
+Note: Assume that Chef and Chefina were born on same day and same month (just different year).
+"""
+
+for _ in range(int(input())):
+    print(int(input()) - 10)
