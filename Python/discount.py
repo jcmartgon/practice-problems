2051f04ea4bf1b29618131f1cdb8cf6767f460bb
JCMartGon <jcmartgon@gmail.com>
2023-08-19 23:38:36 -0600

    Day 79

---
+++ Python/discount.py
@@ -0,0 +1,11 @@
+# Jesus Carlos Martinez Gonzalez
+# 19/08/2023
+# Discount (https://www.codechef.com/problems/DISCNT)
+
+"""
+Alice buys a toy with a selling price of 100 rupees. There is a discount of percent on the toy. Find the
+amount Alice needs to pay for it.
+"""
+
+for _ in range(int(input())):
+    print(100 - int(input()))
