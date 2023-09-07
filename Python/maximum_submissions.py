a324ef7e7e8e82a3559199892999a2200919eac2
JCMartGon <jcmartgon@gmail.com>
2023-09-06 23:22:47 -0600

    Day 97

---
+++ Python/maximum_submissions.py
@@ -0,0 +1,12 @@
+# Jesus Carlos Martinez Gonzalez
+# 06/09/2023
+# Maximum submissions (https://www.codechef.com/problems/MAXIMUMSUBS)
+
+"""
+A participant can make I submission every 30 seconds. If a contest lasts for X minutes, what is the
+maximum number of submissions that the participant can make during it?
+It is also given that the participant cannot make any submission in the last 5 seconds of the contest.
+"""
+
+for _ in range(int(input())):
+    print(int(input()) * 2)
