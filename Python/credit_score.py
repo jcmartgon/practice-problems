92116b8459a4dca30fffafcb3c64891f0cc520f4
JCMartGon <jcmartgon@gmail.com>
2023-09-15 23:49:18 -0600

    Day 106

---
+++ Python/credit_score.py
@@ -0,0 +1,11 @@
+# Jesus Carlos Martinez Gonzalez
+# 15/09/2023
+# Credit score (https://www.codechef.com/problems/CREDSCORE)
+
+"""
+To access CRED programs, one needs to have a credit score of 750 or more.
+Given that the present credit score is X. determine if one can access CRED programs or not.
+If it is possible to access CRED programs. output YES, otherwise output NO.
+"""
+
+print("YES" if int(input()) >= 750 else "NO")
