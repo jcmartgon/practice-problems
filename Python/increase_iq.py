9566fbc41bc1332a02682a2833c83f0667473ff9
JCMartGon <jcmartgon@gmail.com>
2023-09-24 23:54:46 -0600

    Day 115

---
+++ Python/increase_iq.py
@@ -0,0 +1,16 @@
+# Jesus Carlos Martinez Gonzalez
+# 24/09/2023
+# Increase IQ (https://www.codechef.com/problems/INCRIQ)
+
+"""
+A study has shown that playing a musical instrument helps in increasing one's IQ by 7 points. Chef
+knows he can't beat Einstein in physics, but he wants to try to beat him in an IQ competition.
+You know that Einstein had an IQ of 170, and Chef currently has an IQ of X.
+Determine if, after learning to play a musical instrument, Chefs IQ will become strictly greater than
+Einstein's.
+Print "Yes" if it is possible for Chef to beat Einstein, else print "No" (without quotes).
+You may print each character of the string in either uppercase or lowercase (for example, the strings
+YES, yes. Yes, and YES will all be treated as identical).
+"""
+
+print("Yes" if int(input()) + 7 > 170 else "No")
