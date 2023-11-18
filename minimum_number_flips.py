9b56629dca0ca490ab96c32d526e61fe3fd5d8a4
JCMartGon <jcmartgon@gmail.com>
2023-11-17 23:54:25 -0600

    Day 168

---
+++ minimum_number_flips.py
@@ -0,0 +1,20 @@
+# Jesus Carlos Martinez Gonzalez
+# 17/11/2023
+# Minimum number of flips (https://www.codechef.com/problems/MINFLIPS)
+
+"""
+Chef has an array A of length N consisting of I and —I only.
+In one operation, Chef can choose any index i (1 i N) and multiply the element Ai by —1.
+Find the minimum number of operations required to make the sum of the array equal to 0. Output -1
+if the sum of the array cannot be made O.
+"""
+
+
+for _ in range(int(input())):
+    n = int(input())
+    values = list(map(int, input().split()))
+
+    if n & 1 == 1:
+        print(-1)
+    else:
+        print(abs(sum(values)) // 2)
