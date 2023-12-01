f38fe084d8f14aa56b626616db638dfb8326479d
JCMartGon <jcmartgon@gmail.com>
2023-11-30 22:22:49 -0600

    Day 181

--- equal_integers.py
+++ equal_integers.py
@@ -1,21 +1,24 @@
 # Jesus Carlos Martinez Gonzalez
-# 29/11/2023
+# 30/11/2023
 # Equal integers (https://www.codechef.com/problems/INCREAR)
 
 """
 Chef has two integers X and Y. Chefwantsto perform some operations to make X and Y equal. In
 one operation, Chef can either:
-• set
-• or set Y Y +2
+
+- set x to x + 1
+- set y to y + 1
+
 Find the minimum number of operations required to make X and Y equal.
 """
 
 for _ in range(int(input())):
     x, y = map(int, input().split())
-    operations = 0
 
-    while x > y:
-        y += 2
-        operations += 1
+    if x > y:
+        y_operations = (x - y) // 2
+        y += y_operations * 2
 
-    print(operations + y - x)
+        print(y_operations + x - y if x == y else y_operations + x - y + 1)
+    else:
+        print(y - x)
