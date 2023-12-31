362dc85294045e5b5981ffaa0608268a7eb62513
JCMartGon <jcmartgon@gmail.com>
2023-12-30 23:47:09 -0600

    Day 210

---
+++ chef_and_battery.py
@@ -0,0 +1,12 @@
+# Jesus Carlos Martinez Gonzalez
+# 30/12/2023
+# Chef and batter (https://www.codechef.com/problems/FIFTYPE)
+
+"""
+Chefs phone has a battery level of N percent.
+Each minute:
+• Ifthe phone is on charging. the battery level increases by 2%.
+• Otherwise, the battery level decreases by 3%.
+Find the minimum time in which Chef can make the battery level reach exactly 50%.
+Note that the battery level should always lie in the range 0 to 100 (both included).
+"""
