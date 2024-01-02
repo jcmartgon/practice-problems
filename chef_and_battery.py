e11061a7c0b4a9a99111abc550463636a3031adf
JCMartGon <jcmartgon@gmail.com>
2024-01-02 17:28:16 -0600

    Day 213, first of the year!

--- chef_and_battery.py
+++ chef_and_battery.py
@@ -1,5 +1,5 @@
 # Jesus Carlos Martinez Gonzalez
-# 30/12/2023
+# 02/01/2024
 # Chef and batter (https://www.codechef.com/problems/FIFTYPE)
 
 """
@@ -11,4 +11,26 @@ Find the minimum time in which Chef can make the battery level reach exactly 50%
 Note that the battery level should always lie in the range 0 to 100 (both included).
 """
 
-print("That was new year's eve")
+for _ in range(int(input())):
+    charge = int(input())
+    minutes = 0
+
+    while charge != 50:
+        if charge > 50:
+            q, r = divmod(charge - 50, 3)
+
+            if r:
+                q += 1
+
+            charge -= 3 * q
+        else:
+            q, r = divmod(50 - charge, 2)
+
+            if r:
+                q += 1
+
+            charge += 2 * q
+
+        minutes += q
+
+    print(minutes)
