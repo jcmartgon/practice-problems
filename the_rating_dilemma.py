d1330cb6e534578d93e50617bb23d08fab6808d0
JCMartGon <jcmartgon@gmail.com>
2024-01-08 23:26:12 -0600

    Day 219

---
+++ the_rating_dilemma.py
@@ -0,0 +1,16 @@
+# Jesus Carlos Martinez Gonzalez
+# 08/01/2024
+# The rating dilemma (https://www.codechef.com/problems/RATING?tab=statement)
+
+"""
+Chef really likes to compete on Codechef, and he has gained an impressive rating of X. where X > 0.
+There is also a parallel universe, where ratings on Codechef are negative instead. The only thing that
+remains unchanged in the parallel universe is Chef's love for competing on Codechef. Chef's rating on
+Codechef in the parallel universe is Y, where Y < 0.
+Due to some cosmic event, the parallel universe has been destroyed, resulting in Chef forgetting both
+X and Y. He only remembers the sum S = X + Y. He wonders what the maximum possible
+product of his ratings is, given that he knows the sum of his ratings.
+"""
+
+for _ in range(int(input())):
+    print(-int(input()) - 1)
