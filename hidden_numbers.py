1451c6faf31cc3bcf9b7ad78fd673d825af55c2e
JCMartGon <jcmartgon@gmail.com>
2024-01-10 22:42:57 -0600

    Day 220

---
+++ hidden_numbers.py
@@ -0,0 +1,14 @@
+# Jesus Carlos Martinez Gonzalez
+# 09/01/2024
+# Hidden numbers (https://www.codechef.com/problems/UWCOI21A)
+
+"""
+Salmon has some hidden numbers that he wants you to find!
+Given an integer N, find and output any two integers A and B such that:
+• < 109, and
+"""
+
+for _ in range(int(input())):
+    n = int(input())
+
+    print(1, n)
