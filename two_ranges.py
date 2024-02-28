7d0c0b98063f977b376bd5bc2b99b4c47c93047d
JCMartGon <jcmartgon@gmail.com>
2024-02-27 21:12:00 -0600

    Day 2: I'm not happy with this O(n), but it works.

---
+++ two_ranges.py
@@ -0,0 +1,18 @@
+# Jesus Carlos Martinez Gonzalez
+# 27/02/2024
+# Problem (https://www.codechef.com/problems/TWORANGES?tab=statement)
+
+"""
+Chef has two ranges [A, B] and [C, D]. Chef needs to find the number of integers that belong to at
+least one of the ranges.
+Note: A range [P, Q] contains all the integers {P, P + 1, P + 2,... ,
+"""
+
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split())
+
+    lst = list(range(a, b + 1))
+    lst.append(list(range(c, d + 1)))
+    st = set(lst)
+
+    print(len(st))
