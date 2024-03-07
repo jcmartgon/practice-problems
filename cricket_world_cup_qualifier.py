3d06bba8ecef5e54c2f52dc713b6c1ef2d7ded77
JCMartGon <jcmartgon@gmail.com>
2024-03-06 23:04:53 -0600

    Day 9

---
+++ cricket_world_cup_qualifier.py
@@ -0,0 +1,13 @@
+# Jesus Carlos Martinez Gonzalez
+# 06/03/2024
+# Cricket world cup qualifier (https://www.codechef.com/problems/CWC23QUALIF)
+
+"""
+The cricket World Cup has started in Chefland. There are many teams participating in the group stage
+matches. Any team that scores 12 or more points in the group stage matches qualifies for the next
+stage.
+You know the score that a particular team has scored in the group stage matches. Determine if the
+team has qualified for the next stage or not.
+"""
+
+print("YES" if int(input()) >= 12 else "NO")
