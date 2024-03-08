707fdf6a8ff2e101d771feaae3844f457d0b6a39
JCMartGon <jcmartgon@gmail.com>
2024-03-07 23:21:31 -0600

    Day 10

---
+++ christmas_greetings.py
@@ -0,0 +1,11 @@
+# Jesus Carlos Martinez Gonzalez
+# 07/03/2024
+# Christmas greetings (https://www.codechef.com/problems/CHRISTGREET)
+
+"""
+Christmas is celebrated on 25th December every year.
+You are given a day X in December. Determine whether it is Christmas.
+Print CHRISTMAS if it is Christmas. otherwise print ORDINARY.
+"""
+
+print("CHRISTMAS" if int(input()) == 25 else "ORDINARY")
