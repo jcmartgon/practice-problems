d7084d15d8f100f1a042dfe715af783aff98373e
JCMartGon <jcmartgon@gmail.com>
2024-03-08 22:41:37 -0600

    Day 11

---
+++ lucky_seven.py
@@ -0,0 +1,12 @@
+# Jesus Carlos Martinez Gonzalez
+# 08/03/2024
+# Lucky seven (https://www.codechef.com/problems/LUCKYSEVEN)
+
+"""
+Chef considers the number 7 lucky. As a result. he believes that the 7-th letter he sees on a day is his
+lucky letter of the day.
+You are given a string S of length 10, denoting the first 10 letters Chef saw today.
+What is Chefs lucky letter?
+"""
+
+print(input()[6])
