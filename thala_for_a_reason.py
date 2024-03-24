1bedc41466892e19163a7bdcde6ae24b10f75dd4
JCMartGon <jcmartgon@gmail.com>
2024-03-23 23:57:57 -0600

    Day 25

---
+++ thala_for_a_reason.py
@@ -0,0 +1,10 @@
+# Jesus Carlos Martinez Gonzalez
+# 23/03/2024
+# Thala for a reason (https://www.codechef.com/problems/THALA7)
+
+"""
+You are given an integer N.
+Print THALA if N = 7, otherwise print SADGE.
+"""
+
+print("THALA" if int(input()) == 7 else "SADGE")
