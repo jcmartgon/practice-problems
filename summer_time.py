754d25bd4a95924de8be7884e6b6c0a5490c34e8
JCMartGon <jcmartgon@gmail.com>
2024-03-25 23:25:40 -0600

    Day 26

---
+++ summer_time.py
@@ -0,0 +1,12 @@
+# Jesus Carlos Martinez Gonzalez
+# 24/03/2024
+# Summer time (https://www.codechef.com/problems/MANGOLASSI)
+
+"""
+Mamalesh likes to drink mango lassi when it's hot, and only when it's hot. If (and only if) the
+temperature on a given day is strictly greater than 35 degrees, Mamalesh will drink mango lassi.
+Mamalesh sees that today's temperature is X degrees Celsius. Will he drink mango lassi today?
+Print "Yes" if he will. and "No" otherwise (without quotes).
+"""
+
+print("YES" if int(input()) > 35 else "NO")
