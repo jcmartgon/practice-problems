408a2d5bae9824c58990e5101c16805fbd798484
JCMartGon <jcmartgon@gmail.com>
2024-03-27 23:52:17 -0600

    Day 28

---
+++ ac_please.py
@@ -0,0 +1,12 @@
+# Jesus Carlos Martinez Gonzalez
+# 27/03/2024
+# AC Please (https://www.codechef.com/problems/ACPLZ)
+
+"""
+Chef is feeling the heat this summer and wants to switch on the Air Conditioning all the time, but since
+he doesn't want to harm the environment much, he only switches it on when the room temperature is
+strictly greater than 30 degrees celsius.
+Given the room temperature, you need to find whether Chef will switch on the air conditioning or not.
+"""
+
+print("YES" if int(input()) > 30 else "NO")
