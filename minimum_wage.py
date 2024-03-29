a1760ec3770440007efe2133fd66fe4f8ed92fb3
JCMartGon <jcmartgon@gmail.com>
2024-03-28 23:57:47 -0600

    Day 29

---
+++ minimum_wage.py
@@ -0,0 +1,11 @@
+# Jesus Carlos Martinez Gonzalez
+# 27/03/2024
+# Minimum wage (https://www.codechef.com/problems/MINWAGE)
+
+"""
+The minimum wage in Chefland is 11 dollars per hour.
+Given that Chef earns X dollars per hour, find whether his income is strictly above the minimum
+wage.
+"""
+
+print("YES" if int(input()) > 11 else "NO")
