bd2e130d0b1600fcf7d9d19ff7364d706f1afef6
JCMartGon <jcmartgon@gmail.com>
2024-03-30 23:59:08 -0600

    Day 31

---
+++ the_ides_of_march.py
@@ -0,0 +1,13 @@
+# Jesus Carlos Martinez Gonzalez
+# 29/03/2024
+# The ides of march (https://www.codechef.com/problems/IDESM)
+
+"""
+Beware the ides of March.
+Julius Caesar was warned by a soothsayer to be wary of the ides of March — namely, the 15-th of
+March.
+Today is the N-th day of March. Your task is to tell Caesar whether it is the ides of March, so that he
+can take extra safety precautions if necessary.
+"""
+
+print("YES" if input() == "15" else "NO")
