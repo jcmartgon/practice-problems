b9c63d87af08579f5af4dd64be16106b3d6cf888
JCMartGon <jcmartgon@gmail.com>
2024-04-03 23:46:36 -0600

    Day 35

---
+++ sports_sectiony.py
@@ -0,0 +1,11 @@
+# Jesus Carlos Martinez Gonzalez
+# 03/04/2024
+# Sports section (https://www.codechef.com/problems/NEWSPAPER)
+
+"""
+The newspaper in Chefland consists of IO pages numbered I to IO.
+The last 3 pages of the newspaper are always dedicated to the sports section.
+Given a random page number X, determine whether that page is dedicated to the sports section.
+"""
+
+print("YES" if int(input()) > 7 else "NO")
