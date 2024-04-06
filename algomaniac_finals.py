73d40a48dc1167a9e5a3def37b80fbd8de583a4a
JCMartGon <jcmartgon@gmail.com>
2024-04-05 23:27:07 -0600

    Day 37

---
+++ algomaniac_finals.py
@@ -0,0 +1,11 @@
+# Jesus Carlos Martinez Gonzalez
+# 05/04/2024
+# Algomaniac finals (https://www.codechef.com/problems/ALGOFINALS)
+
+"""
+Algomaniac finals. a part of Convolution Fest of Jadavpur University, will be held on March 17.
+Shreyan can only go to Jadavpur University on March X.
+Print YAY if he can attend the Algomaniac finals and NO if he cannot.
+"""
+
+print("YAY" if input() == "17" else "NO")
