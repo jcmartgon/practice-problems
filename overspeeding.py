c0f4a84cc12519eb844ba983c002d19b11f6aeac
JCMartGon <jcmartgon@gmail.com>
2024-04-06 23:27:56 -0600

    Day 38

---
+++ overspeeding.py
@@ -0,0 +1,12 @@
+# Jesus Carlos Martinez Gonzalez
+# 06/04/2024
+# Overspeeding (https://www.codechef.com/problems/CCOV)
+
+"""
+Alice is going for a drive. During her drive, she reached a maximum speed of S km/hr.
+As per the rules of the government. the speed of the vehicle must not exceed 40 km/hr, otherwise the
+person will be fined.
+You need to tell whether Alice will be fined or not.
+"""
+
+print("YES" if int(input()) > 40 else "NO")
