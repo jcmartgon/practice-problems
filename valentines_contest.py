9263aa0851849821533a24d3742323b577949f56
JCMartGon <jcmartgon@gmail.com>
2024-04-09 23:03:48 -0600

    Day 41

---
+++ valentines_contest.py
@@ -0,0 +1,11 @@
+# Jesus Carlos Martinez Gonzalez
+# 09/04/2024
+# Valentines contest (https://www.codechef.com/problems/VAL114)
+
+"""
+Chef knows that a starters will be organised on next Wednesday, i.e., Valentine's day.
+Since it is Starters 120 today, starters 121 is likely to be organised on Valentine's day.
+Given an integer N, find whether starters N is likely to be organised on Valentine's day.
+"""
+
+print("Likely" if input() == "121" else "Unlikely")
