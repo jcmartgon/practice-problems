aa363e88ce8ddb2f8d11a44352d8b14d021bd05c
JCMartGon <jcmartgon@gmail.com>
2024-04-13 23:54:48 -0600

    Day 45

---
+++ air_quality_index.py
@@ -0,0 +1,12 @@
+# Jesus Carlos Martinez Gonzalez
+# 13/04/2024
+# Air quality index (https://www.codechef.com/problems/AIRINDEX)
+
+"""
+In the light of G-2Ø summit, government has decided to keep the average air quality index (AQI) strictly
+below 100.
+On some random day, Chef measures the AQI and found the value to be X.
+Find whether the government was able to keep the AQI within limits.
+"""
+
+print("YES" if int(input()) < 100 else "NO")
